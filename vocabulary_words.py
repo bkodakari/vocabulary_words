6bc9736cfb92dca8e333a571b161ba428aff91fc
Hackbright Student <no-reply@hackbrightacademy.com>
2017-02-06 21:06:36 -0800

    dictionary practice

---
+++ vocabulary_words.py
@@ -0,0 +1,19 @@
+# vocabulary_words = {
+#     "string": "sequence of characters",
+#     "variable": "identifier",
+#     "float": "number with decimal point",
+#     "integer": "whole number",
+#     }
+
+# print vocabulary_words
+
+
+prices = {"banana": 4, "apple": 2, "orange": 1.5, "pear": 3}
+
+
+# def price_of_foods:
+
+
+for my_key, my_value in prices.items():
+    if my_value == max(prices.values()):
+        print my_key
